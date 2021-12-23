3b4db0fecd956480ee31b36c73f324e1f46f7db6
GrittyCode <kanghan9830@gmail.com>
2021-12-23 16:43:33 +0900

    number theory study
    number theory study

---
+++ number theory/5086.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(false);
+
+
+    int a,b;
+    while (1)
+    {
+        cin >> a >> b;
+
+        if(a == 0 && b == 0)
+            break;
+
+        if(b%a == 0)
+        {
+            cout << "factor" << "\n";
+        }
+        else if(a%b == 0)
+        {
+            cout << "multiple" << "\n";
+        }
+        else
+        {
+            cout << "neither" << "\n";
+        }
+    }
+
+    return 0;
+    
+}
